8e0da2e00a1a4ec112fca10c19443414b1544381
varakarapureddy <99267924+varakarapureddy@users.noreply.github.com>
2022-02-24 00:10:12 +0530

    done assigment2

---
+++ assigment2.py
@@ -0,0 +1,12 @@
+# print reverse the list
+l=[]
+l1=[]
+n=int(input("Enter the n: "))
+for i in range(0,n):
+    x=int(input())
+    l.append(x)
+for i in range(0,n):
+    j=i+1
+    l1.insert(-i,l[i])
+print(l)
+print(l1)
